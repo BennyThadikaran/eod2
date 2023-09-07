655ebb51033cf50917034cd90fa67a744ac33772
Benny Thadikaran <tbenny05@yahoo.com>
2023-09-07 23:09:43 +0530

    - Added support for plotting moving average of volume. - Added keyboard shortcuts to navigate charts directly from matplotlib figure. - Added mouse and keyboard interaction for drawing various lines. - Added new Magnet mode to snap lines to nearest Open, High, Low, Close - Added experimental plugin support for adding custom indicators. - Added new plugin RSI indicator. - Added support for storing user drawn lines and restoring them on demand. - Added improved OHLCV readout in top right corner of matplotlib figure. - Added config variables to customise line color. - Fix: preset not updating chart timeframe, when tf is set to 'weekly' - Added type hints to fix pyright warnings. - Drop prompt navigation interface in favor of direct interaction with matplotlib figure.

---
+++ src/defs/Plugin.py
@@ -0,0 +1,17 @@
+from importlib import import_module
+from argparse import ArgumentParser
+
+
+class Plugin:
+    def __init__(self):
+        self.plugins = []
+
+    def register(self, plugins: dict, parser: ArgumentParser):
+        for plugin in plugins.values():
+            instance = import_module(f'plugin.{plugin["name"]}')
+            instance.load(parser)
+            self.plugins.append(instance)
+
+    def run(self, *args):
+        for plugin in self.plugins:
+            plugin.main(*args)
---
+++ src/plugin/rsi.py
@@ -0,0 +1,51 @@
+from argparse import ArgumentParser
+from ta.momentum import RSIIndicator
+from mplfinance import make_addplot
+from pandas import Series
+
+# To be added to src/defs/user.json
+# "PLOT_PLUGINS": {
+#     "RSI": {
+#       "name": "rsi",
+#       "overbought": 80,
+#       "oversold": 20,
+#       "line_color": "teal"
+#     }
+# }
+
+
+def load(parser: ArgumentParser):
+    parser.add_argument('--rsi', action='store_true',
+                        help='Relative strength index')
+
+
+def main(df, plot_args, args, config):
+    if args.rsi:
+        opts = config.PLOT_PLUGINS['RSI']
+
+        df['RSI'] = RSIIndicator(close=df['Close']).rsi()
+
+        if not 'addplot' in plot_args:
+            plot_args['addplot'] = []
+
+        OB_LINE = Series(data=opts['overbought'], index=df.index)
+        OS_LINE = Series(data=opts['oversold'], index=df.index)
+
+        plot_args['addplot'].extend([
+            make_addplot(df['RSI'],
+                         label='RSI',
+                         panel='lower',
+                         color=opts['line_color'],
+                         ylabel='RSI',
+                         width=2),
+            make_addplot(OB_LINE,
+                         panel='lower',
+                         color=opts['line_color'],
+                         linestyle='dashed',
+                         width=1.5),
+            make_addplot(OS_LINE,
+                         panel='lower',
+                         color=opts['line_color'],
+                         linestyle='dashed',
+                         width=1.5)
+        ])
