b4f03566165004dacac13894afaed0f781a0e1b5
Benny Thadikaran <tbenny05@yahoo.com>
2023-11-04 01:02:17 +0530

    Update to Version 5.0.0

--- src/defs/Config.py
+++ src/defs/Config.py
@@ -140,7 +140,7 @@ class Config:
             self.__dict__.update(dct)
 
     # DO NOT EDIT BELOW
-    VERSION = '4.0'
+    VERSION = '5.0.0'
 
     def toList(self, filename: str):
         return (DIR / 'data' / filename).read_text().strip().split("\n")
