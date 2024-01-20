794c7eeedf04f10d2e69d828015547df331afd05
Benny Thadikaran <tbenny05@yahoo.com>
2024-01-20 23:09:35 +0530

    - Updated eod2_data submodule - formatting changes -

--- src/init.py
+++ src/init.py
@@ -4,7 +4,7 @@ try:
     from nse import NSE
 except ModuleNotFoundError:
     # Inform user to install nse.
-    pip = 'pip' if 'win' in platform else 'pip3'
+    pip = "pip" if "win" in platform else "pip3"
 
     exit(f"EOD2 requires 'nse' package. Run '{pip} install -U nse'")
 
@@ -12,24 +12,22 @@ from defs.utils import writeJson
 from defs import defs
 from argparse import ArgumentParser
 
-parser = ArgumentParser(prog='init.py')
+parser = ArgumentParser(prog="init.py")
 
 group = parser.add_mutually_exclusive_group()
 
-group.add_argument('-v',
-                   '--version',
-                   action='store_true',
-                   help='Print the current version.')
+group.add_argument(
+    "-v", "--version", action="store_true", help="Print the current version."
+)
 
-group.add_argument('-c',
-                   '--config',
-                   action='store_true',
-                   help='Print the current config.')
+group.add_argument(
+    "-c", "--config", action="store_true", help="Print the current config."
+)
 
 args = parser.parse_args()
 
 if args.version:
-    exit(f'EOD2 init.py: version {defs.config.VERSION}')
+    exit(f"EOD2 init.py: version {defs.config.VERSION}")
 
 if args.config:
     exit(str(defs.config))
@@ -39,11 +37,11 @@ nse = NSE(defs.DIR)
 if defs.config.AMIBROKER and not defs.isAmiBrokerFolderUpdated():
     defs.updateAmiBrokerRecords(nse)
 
-if 'DLV_PENDING_DATES' not in defs.meta:
-    defs.meta['DLV_PENDING_DATES'] = []
+if "DLV_PENDING_DATES" not in defs.meta:
+    defs.meta["DLV_PENDING_DATES"] = []
 
-if len(defs.meta['DLV_PENDING_DATES']):
-    pendingList = defs.meta['DLV_PENDING_DATES'].copy()
+if len(defs.meta["DLV_PENDING_DATES"]):
+    pendingList = defs.meta["DLV_PENDING_DATES"].copy()
 
     for dateStr in pendingList:
         if defs.updatePendingDeliveryData(nse, dateStr):
@@ -61,7 +59,7 @@ while True:
     defs.validateNseActionsFile(nse)
 
     # Download all files and validate for errors
-    print('Downloading Files')
+    print("Downloading Files")
 
     try:
         # NSE bhav copy
@@ -77,28 +75,28 @@ while True:
         # NSE delivery
         DELIVERY_FILE = nse.deliveryBhavcopy(defs.dates.dt)
     except (RuntimeError, Exception):
-        defs.meta['DLV_PENDING_DATES'].append(defs.dates.dt.isoformat())
+        defs.meta["DLV_PENDING_DATES"].append(defs.dates.dt.isoformat())
         DELIVERY_FILE = None
-        print('Delivery Report Unavailable. Will retry in subsequent sync')
+        print("Delivery Report Unavailable. Will retry in subsequent sync")
 
     try:
-        print('Starting Data Sync')
+        print("Starting Data Sync")
 
         defs.updateNseEOD(BHAV_FILE, DELIVERY_FILE)
 
-        print('EOD sync complete')
+        print("EOD sync complete")
 
         # INDEX sync
         defs.updateIndexEOD(INDEX_FILE)
 
-        print('Index sync complete.')
+        print("Index sync complete.")
     except Exception as e:
         # rollback
         print(f"Error during data sync. {e!r}")
         defs.rollback(defs.DAILY_FOLDER)
         defs.cleanup((BHAV_FILE, DELIVERY_FILE, INDEX_FILE))
 
-        defs.meta['lastUpdate'] = defs.dates.lastUpdate
+        defs.meta["lastUpdate"] = defs.dates.lastUpdate
         writeJson(defs.META_FILE, defs.meta)
         nse.exit()
         exit()
@@ -106,7 +104,7 @@ while True:
     # No errors continue
 
     # Adjust Splits and bonus
-    print('Makings adjustments for splits and bonus')
+    print("Makings adjustments for splits and bonus")
 
     try:
         defs.adjustNseStocks()
@@ -118,17 +116,17 @@ while True:
         defs.rollback(defs.DAILY_FOLDER)
         defs.cleanup((BHAV_FILE, DELIVERY_FILE, INDEX_FILE))
 
-        defs.meta['last_update'] = defs.dates.lastUpdate
+        defs.meta["last_update"] = defs.dates.lastUpdate
         writeJson(defs.META_FILE, defs.meta)
         nse.exit()
         exit()
 
-    print('Cleaning up files')
+    print("Cleaning up files")
 
     defs.cleanup((BHAV_FILE, DELIVERY_FILE, INDEX_FILE))
     defs.cleanOutDated()
 
-    defs.meta['lastUpdate'] = defs.dates.lastUpdate = defs.dates.dt
+    defs.meta["lastUpdate"] = defs.dates.lastUpdate = defs.dates.dt
     writeJson(defs.META_FILE, defs.meta)
 
     print(f'{defs.dates.dt:%d %b %Y}: Done\n{"-" * 52}')
