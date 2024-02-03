746a8a73a8902c3b0d048f254c9f62f9475885b5
Benny Thadikaran <tbenny05@yahoo.com>
2024-02-03 13:08:22 +0530

    - Display Nifty PE on every sync - Updated eod2_data submodule

--- src/defs/Config.py
+++ src/defs/Config.py
@@ -137,7 +137,7 @@ class Config:
             self.__dict__.update(dct)
 
     # DO NOT EDIT BELOW
-    VERSION = "5.1.3"
+    VERSION = "5.1.4"
 
     def toList(self, filename: str):
         return (DIR / "data" / filename).read_text().strip().split("\n")
