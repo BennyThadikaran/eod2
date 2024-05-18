92c1de1e988e7fcc7a3552778ecd18785448466b
Benny Thadikaran <tbenny05@yahoo.com>
2024-05-18 22:33:01 +0530

    Bump to version 5.2.1

--- src/defs/Config.py
+++ src/defs/Config.py
@@ -137,7 +137,7 @@ class Config:
             self.__dict__.update(dct)
 
     # DO NOT EDIT BELOW
-    VERSION = "5.2.0"
+    VERSION = "5.2.1"
 
     def toList(self, filename: str):
         return (DIR / "data" / filename).read_text().strip().split("\n")
