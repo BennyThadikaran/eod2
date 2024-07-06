e02d497f2a2706855249d17916b1796a78324089
Benny Thadikaran <tbenny05@yahoo.com>
2024-07-06 12:43:07 +0530

    Bump to version 6.0.2

--- src/defs/Config.py
+++ src/defs/Config.py
@@ -138,7 +138,7 @@ class Config:
             self.__dict__.update(dct)
 
     # DO NOT EDIT BELOW
-    VERSION = "6.0.1"
+    VERSION = "6.0.2"
 
     def toList(self, filename: str):
         return (DIR / "data" / filename).read_text().strip().split("\n")
