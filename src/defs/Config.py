cec9fa0f7737a564d78beb2d241a1d4f0d09a332
Benny Thadikaran <tbenny05@yahoo.com>
2025-05-10 14:47:29 +0530

    Bump to version 8.0.0

--- src/defs/Config.py
+++ src/defs/Config.py
@@ -84,7 +84,7 @@ class Config:
             self.__dict__.update(dct)
 
     # DO NOT EDIT BELOW
-    VERSION = "7.0.13"
+    VERSION = "8.0.0"
 
     def toList(self, filename: str):
         return (DIR / "data" / filename).read_text().strip().split("\n")
