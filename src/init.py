4fe9b10cf120e97fbc099b6da9969f49a129a576
Benny Thadikaran <tbenny05@yahoo.com>
2026-07-11 15:05:39 +0530

    Add check for nse version 3.1.1 in init.py

--- src/init.py
+++ src/init.py
@@ -13,8 +13,8 @@ logger = logging.getLogger(__name__)
 
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
-if not defs.is_version_compatible(NSE.__version__, major=1, minor=2, patch=4):
-    logger.warning("Require NSE version 1.2.*. Run `pip install 'nse[server]==1.2.9'`")
+if not defs.is_version_compatible(NSE.__version__, major=3, minor=1, patch=1):
+    logger.warning("Require NSE version 3.1.*. Run `pip install 'nse[server]==3.1.1'`")
     exit(1)
 
 data_version = defs.meta.get("data-version", None)
