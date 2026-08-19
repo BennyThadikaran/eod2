fca27951de313184e35f941714e6c55ba3783160
Benny Thadikaran <tbenny05@yahoo.com>
2026-08-19 18:36:44 +0530

    Bump to version 9.4.5

--- src/defs/config.py
+++ src/defs/config.py
@@ -86,7 +86,7 @@ class Config:
     PRESET: dict[str, Any] = field(default_factory=dict)
 
     # ---------- INTERNAL ----------
-    VERSION: str = "9.4.4"
+    VERSION: str = "9.4.5"
     EXPECTED_DATA_VERSION: float = 3.4
 
     @classmethod
