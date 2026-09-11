f099455d7e5ac7189752c6ae17d77e654ae74fa0
Benny Thadikaran <tbenny05@yahoo.com>
2026-09-11 23:41:33 +0530

    fix: handle whitespace in NSE delivery columns, simplify series filtering

--- src/defs/defs.py
+++ src/defs/defs.py
@@ -569,15 +569,12 @@ def updateNseEOD(bhavFile: Path, deliveryFile: Optional[Path]):
 
         dlvDf.to_csv(DLV_FOLDER / deliveryFile.name)
 
+        dlvDf.columns = dlvDf.columns.str.strip()
+        dlvDf.SERIES = dlvDf.SERIES.str.strip()
+
         # filter the pd.DataFrame for stocks series EQ, BE and BZ
         # https://www.nseindia.com/market-data/legend-of-series
-        dlvDf = dlvDf[
-            (dlvDf[" SERIES"] == " EQ")
-            | (dlvDf[" SERIES"] == " BE")
-            | (dlvDf[" SERIES"] == " BZ")
-            | (dlvDf[" SERIES"] == " SM")
-            | (dlvDf[" SERIES"] == " ST")
-        ]
+        dlvDf = dlvDf[dlvDf.SERIES.isin(["EQ", "BE", "BZ", "SM", "ST"])]
     else:
         dlvDf = None
 
@@ -589,7 +586,7 @@ def updateNseEOD(bhavFile: Path, deliveryFile: Optional[Path]):
 
         if dlvDf is not None:
             if t.TckrSymb in dlvDf.index:
-                trdCnt, dq = dlvDf.loc[t.TckrSymb, [" NO_OF_TRADES", " DELIV_QTY"]]
+                trdCnt, dq = dlvDf.loc[t.TckrSymb, ["NO_OF_TRADES", "DELIV_QTY"]]
 
                 # BE and BZ series stocks are all delivery trades,
                 # so we use the volume
